eea971f636f05e27e0a31005f41ac7c69a99df7b
Caambodia <36169027+Caambodia@users.noreply.github.com>
2019-04-25 14:01:47 -0400

    Update final_irec.ino

--- final_irec.ino
+++ final_irec.ino
@@ -1,36 +1,44 @@
+// import libraries for sensors
 #include <SPI.h>
 #include <SD.h>
 #include <DHT.h>
 
+// instantiate variables
 float temp;
 float humid;
 int timeS;
 
-#define DHTPIN 7     // what pin we're connected to - Digital 7
-#define DHTTYPE DHT22   // DHT 22  (AM2302)
-DHT dht(DHTPIN, DHTTYPE); //// Initialize DHT sensor for normal 16mhz Arduino
+// set pins
+#define DHTPIN 7                                // Temp / Humidity Sensor - "PWM" pin 7
+#define DHTTYPE DHT22                           // DHT 22 (AM2302)
+DHT dht(DHTPIN, DHTTYPE);                       // initialize DHT sensor for normal 16mhz Arduino
+
+// particulate matter sensor
 unsigned long duration;
 unsigned long starttime;
-unsigned long sampletime_ms = 2000; 
+unsigned long sampletime_ms = 2000;
 unsigned long lowpulseoccupancy = 0;
 float ratio = 0;
 float concentration = 0;
-int pin = 8;
-int MQ2 = A0;
+int pin = 8;                                    // "PWM" pin 8
+
+int MQ2 = A1;                                   // MQ2 - Analog pin 1
+int MQ9 = A0;                                   // MQ9 - Analog pin 0
+
 File myFile;
 
 void setup() {
   // Open serial communications and wait for port to open:
-  Serial.begin(9600);
-  dht.begin();
+  Serial.begin(9600);                           // 9600 baud rate
+  dht.begin();                                  // initialize dht sensor
   while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB port only
   }
 
-  Serial.print("Initializing SD card...");
+  Serial.print("Initializing SD card...");      // update message
 
   if (!SD.begin(53)) {
-    Serial.println("initialization failed!");
+    Serial.println("initialization failed!");   // failure message
     while (1);
   }
   Serial.println("initialization done.");
@@ -43,12 +51,10 @@ void setup() {
   if (myFile) {
     Serial.print("Writing to test.txt...");
     myFile.println("Index, Temperature, Humidity, Carbon Monoxide, Smoke, Particulate Matter");   // print the headings
-    // close the file:
-    myFile.close();
-    Serial.println("done.");
+    myFile.close();                             // close the file
+    Serial.println("done.");                    // closing message
   } else {
-    // if the file didn't open, print an error:
-    Serial.println("error opening test.txt");
+    Serial.println("error opening test.txt");   // if the file didn't open, print an error
   }
 
   // re-open the file for reading:
@@ -60,30 +66,25 @@ void setup() {
     while (myFile.available()) {
       Serial.write(myFile.read());
     }
-    // close the file:
-    myFile.close();
+    myFile.close();                             // close the file
   } else {
-    // if the file didn't open, print an error:
-    Serial.println("error opening test.txt");
+    Serial.println("error opening test.txt");   // if the file didn't open, print an error:
   }
-    timeS = 0;
-
-    pinMode(8,INPUT);
-    starttime = millis(); 
+    timeS = 0;                                  // time variable
+    pinMode(8,INPUT);                           // initialize "PWM" pin 8
+    starttime = millis();
 }
 
 void loop() {
-    int analogSensor = analogRead(MQ2);
+    int MQ2d = analogRead(MQ2);                 // read from the MQ2
+    int MQ9d = analogRead(MQ9);                 // read from the MQ9
     
-    int val;
-    val=analogRead(A0);//Read Gas value from analog 0
+    temp = dht.readTemperature();               // read the temp
+    temp = temp * 1.8 + 32;                     // convert to F
+    humid = dht.readHumidity();                 // read the humidity
     
-    temp = dht.readTemperature();
-    temp = temp * 1.8 + 32;
-    humid = dht.readHumidity();
-    
-    delay(1000); //Delay 2 sec.
-    myFile = SD.open("test.txt", FILE_WRITE);
+    delay(1000);                                // delay before writing
+    myFile = SD.open("test.txt", FILE_WRITE);   // open the text file to write
 
   // if the file opened okay, write to it:
   if (myFile) {
@@ -94,53 +95,43 @@ void loop() {
     myFile.print(", ");
     myFile.print(humid);
     myFile.print(", ");
-    myFile.print(val,DEC);
+    myFile.print(MQ2,DEC);
     myFile.print(", ");
-    myFile.print(analogSensor);
+    myFile.print(MQ9,DEC);
     myFile.print(", ");
     myFile.println(concentration);
     delay(1000);
-    // close the file:
-    myFile.close();
+    myFile.close();                             // close the file
     Serial.println("done.");
   } else {
-    // if the file didn't open, print an error:
-    Serial.println("error opening test.txt");
+    Serial.println("error opening test.txt");   // if the file didn't open, print an error
   }
 
   // re-open the file for reading:
   myFile = SD.open("test.txt");
   if (myFile) {
     Serial.println("test.txt:");
-
     // read from the file until there's nothing else in it:
     while (myFile.available()) {
       Serial.write(myFile.read());
     }
-    // close the file:
-    myFile.close();
+    myFile.close();                             // close the file
   } else {
-    // if the file didn't open, print an error:
-    Serial.println("error opening test.txt");
+    Serial.println("error opening test.txt");   // if the file didn't open, print an error
   }
 
-  timeS++;
-
+  timeS++;                                      // increment time variable
 
   //code for particulate matter sensor
   duration = pulseIn(pin, LOW);
   lowpulseoccupancy = lowpulseoccupancy+duration;
-  if ((millis()-starttime) >= sampletime_ms) //if the sampel time = = 30s
+  if ((millis()-starttime) >= sampletime_ms)    // if the sample time = = 30s
   {
     ratio = lowpulseoccupancy/(sampletime_ms*10.0);  
-    concentration = 1.1*pow(ratio,3)-3.8*pow(ratio,2)+520*ratio+0.62; 
-    //Serial.print("Concentration = ");
-    //Serial.print(concentration);
-    //Serial.println(" pcs/0.01cf");
-    //Serial.println("\n");
+    concentration = 1.1*pow(ratio,3)-3.8*pow(ratio,2)+520*ratio+0.62;
     lowpulseoccupancy = 0;
     starttime = millis();
   }
   
-  delay(2000);
+  delay(2000); // wait until restarting loop
 }
